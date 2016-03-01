192e0bb5f1999ad608f1ab5a253849aebbb5c282
Seth R Johnson <johnsonsr@ornl.gov>
2016-03-01 08:52:19 -0500

    Added multiplication by two test

---
+++ mulbytwo/test.cc
@@ -0,0 +1,18 @@
+//---------------------------------*-C++-*-----------------------------------//
+/*!
+ * \file   mulbytwo/test.cc
+ * \author Seth R Johnson
+ * \date   Mon Feb 15 11:34:12 2016
+ * \brief  test class definitions.
+ * \note   Copyright (c) 2016 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+double mul_by_two(double input)
+{
+    return 2 * input;
+}
+
+//---------------------------------------------------------------------------//
+// end of mulbytwo/test.cc
+//---------------------------------------------------------------------------//
