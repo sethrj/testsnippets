d3ad8513aa9954003585fd4c4b945b45716bef0e
Seth R Johnson <johnsonsr@ornl.gov>
2026-06-11 13:08:15 -0400

    Don't store spans locally due to register increase

--- _celeritas_code/surface-functors/compressed-face.s
+++ _celeritas_code/surface-functors/compressed-face.s
@@ -5,36 +5,39 @@
 do_something(celeritas::OrangeParamsData<(celeritas::Ownership)2, (celeritas::MemSpace)0> const&, celeritas::CompressedFacesRecord const&): ; @do_something(celeritas::OrangeParamsData<(celeritas::Ownership)2, (celeritas::MemSpace)0> const&, celeritas::CompressedFacesRecord const&)
 	.cfi_startproc
 ; %bb.0:
-	sub	sp, sp, #64
-	stp	x20, x19, [sp, #32]             ; 16-byte Folded Spill
-	stp	x29, x30, [sp, #48]             ; 16-byte Folded Spill
-	.cfi_def_cfa_offset 64
+	sub	sp, sp, #80
+	stp	x22, x21, [sp, #32]             ; 16-byte Folded Spill
+	stp	x20, x19, [sp, #48]             ; 16-byte Folded Spill
+	stp	x29, x30, [sp, #64]             ; 16-byte Folded Spill
+	.cfi_def_cfa_offset 80
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
 	.cfi_offset w19, -24
 	.cfi_offset w20, -32
-	ldp	x9, x10, [x1]
-	ldp	x8, x11, [x1, #16]
-	ldr	x12, [x0, #328]
-	add	x12, x12, x8, lsl #3
-	cmp	x8, x11
-	csel	x8, xzr, x12, eq
-	subs	x19, x10, x9
+	.cfi_offset w21, -40
+	.cfi_offset w22, -48
+	ldp	x9, x8, [x1]
+	subs	x20, x8, x9
 	b.eq	LBB0_3
 ; %bb.1:
-	add	x20, x8, #16
+	mov	x19, x0
+	ldr	x8, [x1, #16]
+	lsl	x21, x8, #3
 LBB0_2:                                 ; =>This Inner Loop Header: Depth=1
-	ldp	q0, q1, [x20, #-16]
+	ldr	x8, [x19, #328]
+	add	x8, x8, x21
+	ldp	q0, q1, [x8]
 	stp	q0, q1, [sp]
 	mov	x0, sp
 	bl	void do_something<celeritas::Plane>(celeritas::Plane const&)
-	add	x20, x20, #32
-	subs	x19, x19, #1
+	add	x21, x21, #32
+	subs	x20, x20, #1
 	b.ne	LBB0_2
 LBB0_3:
-	ldp	x29, x30, [sp, #48]             ; 16-byte Folded Reload
-	ldp	x20, x19, [sp, #32]             ; 16-byte Folded Reload
-	add	sp, sp, #64
+	ldp	x29, x30, [sp, #64]             ; 16-byte Folded Reload
+	ldp	x20, x19, [sp, #48]             ; 16-byte Folded Reload
+	ldp	x22, x21, [sp, #32]             ; 16-byte Folded Reload
+	add	sp, sp, #80
 	ret
 	.cfi_endproc
                                         ; -- End function
@@ -48,18 +51,14 @@ do_something(celeritas::OrangeParamsData<(celeritas::Ownership)2, (celeritas::Me
 	.cfi_def_cfa_offset 48
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
-	ldp	x8, x9, [x1, #16]
-	ldr	x10, [x0, #328]
-	add	x10, x10, x8, lsl #3
-	cmp	x8, x9
-	csel	x8, xzr, x10, eq
-	ldp	x9, x10, [x1, #32]
-	ldr	x11, [x0, #344]
-	add	x11, x11, x9, lsl #3
-	cmp	x9, x10
-	csel	x9, xzr, x11, eq
-	ldr	x9, [x9, x2, lsl #3]
-	add	x8, x8, x9, lsl #3
+	ldr	x8, [x1, #32]
+	ldr	x9, [x0, #344]
+	add	x8, x9, x8, lsl #3
+	ldr	x8, [x8, x2, lsl #3]
+	ldr	x9, [x1, #16]
+	add	x8, x9, x8
+	ldr	x9, [x0, #328]
+	add	x8, x9, x8, lsl #3
 	ldp	q0, q1, [x8]
 	stp	q0, q1, [sp]
 	mov	x0, sp
