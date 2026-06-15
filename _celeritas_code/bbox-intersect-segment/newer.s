5f14c0a713d1f74218b3fdc3653d41442d4796b3
Seth R Johnson <johnsonsr@ornl.gov>
2026-06-15 08:04:24 -0400

    Compare with short-circuiting

--- _celeritas_code/bbox-intersect-segment/newer.s
+++ _celeritas_code/bbox-intersect-segment/newer.s
@@ -3,7 +3,7 @@
 
 SYMBOL TABLE:
 0000000000000000 l     F __TEXT,__text ltmp0
-0000000000008208 l     O __LD,__compact_unwind ltmp1
+0000000000008210 l     O __LD,__compact_unwind ltmp1
 0000000000000000 g     F __TEXT,__text isect(celeritas::BoundingBox<double> const&, celeritas::Array<double, 3ul> const&, celeritas::Array<double, 3ul> const&, double)
 
 Disassembly of section __TEXT,__text:
@@ -27,33 +27,33 @@ Disassembly of section __TEXT,__text:
 ;         T const center = (lower + upper) / 2;
 	dup.2d	v3, v1[1]
 	ld1.d	{ v3 }[1], [x0]
-	fadd.2d	v4, v1, v2
-	dup.2d	v5, v2[1]
-	ld1.d	{ v5 }[1], [x8]
-	fadd.2d	v6, v3, v5
+	dup.2d	v4, v2[1]
+	fadd.2d	v5, v1, v2
+	ld1.d	{ v4 }[1], [x8]
+	fadd.2d	v6, v3, v4
 	fmov.2d	v7, #0.50000000
-	fmul.2d	v4, v4, v7
+	fmul.2d	v5, v5, v7
 	fmul.2d	v6, v6, v7
 ; SOURCE/src/orange/BoundingBoxUtils.hh:313
 ;         hw[i] = (upper - lower) / 2;
 	fsub.2d	v1, v2, v1
-	fsub.2d	v2, v5, v3
+	fsub.2d	v2, v4, v3
 	fmul.2d	v1, v1, v7
 	fmul.2d	v2, v2, v7
 ; SOURCE/src/orange/BoundingBoxUtils.hh:314
 ;         hseg[i] = dir[i] * half_distance;
 	ldur	q3, [x2, #8]
-	ldr	d5, [x2, #16]
-	ld1.d	{ v5 }[1], [x2]
+	ldr	d4, [x2, #16]
+	ld1.d	{ v4 }[1], [x2]
 	fmul.2d	v3, v3, v0[0]
-	fmul.2d	v0, v5, v0[0]
+	fmul.2d	v0, v4, v0[0]
 ; SOURCE/src/orange/BoundingBoxUtils.hh:315
 ;         abs_hseg[i] = std::fabs(hseg[i]) + eps;
-	fabs.2d	v5, v3
+	fabs.2d	v4, v3
 	fabs.2d	v7, v0
 	mov	x8, #4372995238176751616 ; =0x3cb0000000000000
 	dup.2d	v16, x8
-	fadd.2d	v5, v5, v16
+	fadd.2d	v4, v4, v16
 	fadd.2d	v7, v7, v16
 ; SOURCE/src/orange/BoundingBoxUtils.hh:316
 ;         mid[i] = pos[i] + hseg[i] - center;
@@ -62,7 +62,7 @@ Disassembly of section __TEXT,__text:
 	ldr	q17, [x1]
 	fadd.2d	v17, v3, v17
 	fadd.2d	v16, v0, v16
-	fsub.2d	v18, v17, v4
+	fsub.2d	v18, v17, v5
 	fsub.2d	v6, v16, v6
 ; SOURCE/src/orange/BoundingBoxUtils.hh:325
 ;         return std::fabs(mid[j] * hseg[k] - mid[k] * hseg[j])
@@ -79,7 +79,7 @@ Disassembly of section __TEXT,__text:
 	fadd	d19, d20, d19
 	fcmp	d16, d19
 	cset	w8, le
-	fadd.2d	v16, v1, v5
+	fadd.2d	v16, v1, v4
 ; SOURCE/src/orange/BoundingBoxUtils.hh:325
 ;         return std::fabs(mid[j] * hseg[k] - mid[k] * hseg[j])
 	fneg.2d	v19, v3
@@ -88,10 +88,10 @@ Disassembly of section __TEXT,__text:
 ; SOURCE/src/orange/BoundingBoxUtils.hh:321
 ;         = [&](int i) { return std::fabs(mid[i]) > hw[i] + abs_hseg[i]; };
 	fabs.2d	v19, v19
-	fabd.2d	v4, v17, v4
+	fabd.2d	v5, v17, v5
 ; SOURCE/src/orange/BoundingBoxUtils.hh:326
 ;                > hw[j] * abs_hseg[k] + hw[k] * abs_hseg[j];
-	fmul.2d	v17, v5, v2
+	fmul.2d	v17, v4, v2
 	fmla.2d	v17, v7, v1
 ; SOURCE/src/orange/BoundingBoxUtils.hh:325
 ;         return std::fabs(mid[j] * hseg[k] - mid[k] * hseg[j])
@@ -103,23 +103,24 @@ Disassembly of section __TEXT,__text:
 ; SOURCE/src/orange/BoundingBoxUtils.hh:326
 ;                > hw[j] * abs_hseg[k] + hw[k] * abs_hseg[j];
 	fmul.d	d1, d1, v7[1]
-	fmla.d	d1, d5, v2[1]
+	fmla.d	d1, d4, v2[1]
 	fcmp	d0, d1
 ; SOURCE/src/orange/BoundingBoxUtils.hh:334
 ;     return !logical_any(found_sep_ortho_axis(x),
 	fcmgt.2d	v0, v19, v17
-	fcmgt.2d	v1, v4, v16
+	fcmgt.2d	v1, v5, v16
 	uzp1.4s	v0, v1, v0
-	umaxv.4s	s0, v0
+	mvn.16b	v0, v0
+	xtn.4h	v0, v0
+	uminv.4h	h0, v0
 	fmov	w9, s0
-	eor	w9, w9, #0x1
 ; SOURCE/src/corecel/math/Algorithms.hh:862
-;     return (false | ... | static_cast<bool>(args));
-	csel	w8, wzr, w8, gt
+;     return (false || ... || static_cast<bool>(args));
+	and	w8, w8, w9
 ; SOURCE/src/orange/BoundingBoxUtils.hh:334
 ;     return !logical_any(found_sep_ortho_axis(x),
-	and	w0, w8, w9
+	csel	w0, wzr, w8, gt
 ; FILE.cc:11
 ; { return intersects_segment(bbox, pos, dir, distance); }
 	ret
-; Total code size:      768
+; Total code size:      776
--- _celeritas_code/build.sh
+++ _celeritas_code/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SOURCE=/Users/seth/Code/celeritas
+SOURCE=/Users/seth/Code/celeritas-temp
 BUILD=$SOURCE/build
 export CXXFLAGS="${CXXFLAGS} -O3 -std=c++20 -I${SOURCE}/src -I${BUILD}/include"
 export CXX=c++
